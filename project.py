50406e6d0743a9ea5b72c184042998c4f961c30b
Hosea174 <hossaena2018@gmail.com>
2022-08-11 19:15:13 +0300

    input validation and colorful terminal

--- project.py
+++ project.py
@@ -1,15 +1,14 @@
 import os
 import re
+import time
 import urllib.request
-from timeit import default_timer as timer
 
-import mutagen
 import spotipy
 from moviepy.editor import *
 from mutagen.easyid3 import EasyID3
 from mutagen.id3 import APIC, ID3
-from mutagen.mp3 import MP3
 from pytube import YouTube
+from rich.console import Console
 from spotipy.oauth2 import SpotifyClientCredentials
 
 SPOTIPY_CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
@@ -22,27 +21,39 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
 def main():
-    # TODO: get this url from the user, and validate it
     # url = "https://open.spotify.com/track/1AI7UPw3fgwAFkvAlZWhE0?si=19066311c4e84d11"
-    url = "https://open.spotify.com/playlist/4GY7mdvKomjSHLZynGeOOZ?si=857f114d0f12416c"
+    # url = "https://open.spotify.com/playlist/4GY7mdvKomjSHLZynGeOOZ?si=857f114d0f12416c"
     # url = "https://open.spotify.com/track/6sy3LkhNFjJWlaeSMNwQ62?si=fdc57ca0a1314817"
+    
+    url = get_url()
     songs = []
     if "track" in url:
         songs.append(get_track_info(url))
     elif "playlist" in url:
         songs.extend(get_playlist_info(url))
 
+    start = time.time()
     for i, track_info in enumerate(songs, start=1):
         search_term = f"{track_info['artist_name']} {track_info['track_title']} audio"
         video_link = find_youtube(search_term)
-        print(f"({i}/{len(songs)}) Downloading ", end="")
+        console.print(f"[magenta]({i}/{len(songs)})[/magenta] Downloading '[cyan]{track_info['artist_name']} - {track_info['track_title']}[/cyan]'...")
+
         download_yt(video_link, track_info)
+    end = time.time()
+
+    console.print(f"DOWNLOAD COMPLETED: {len(songs)} song(s) dowloaded".center(70, " "), style="on green")
+    console.print(f"Time taken: {round(end - start)} sec".center(70, " "), style="on white")
+
+def get_url():
+    url = input("Enter a a spotify url: ")
+    if re.search(r"^(https?://)?open\.spotify\.com/(playlist|track)/.+$", url):
+        return url
+    
+    raise ValueError("Invalid spotify url")
 
 
 def get_track_info(track_url):
     track = sp.track(track_url)
-    with open("response.py", "w") as file:
-        file.write(str(track))
     # TODO: handle request errors
     # TODO: handle potential KeyError and IndexError
     track_metadata = {
@@ -75,30 +86,23 @@ def get_playlist_info(sp_playlist):
 def find_youtube(query):
     # TODO: handle request errors
     # TODO: automatically retry if error is raised after a few seconds
-    start = timer()
     words = query.replace(" ", "+")
     search_link = "https://www.youtube.com/results?search_query=" + words
     response = urllib.request.urlopen(search_link)
     search_results = re.findall(r"watch\?v=(\S{11})", response.read().decode())
     first_vid = "https://www.youtube.com/watch?v=" + search_results[0]
-    end = timer()
-    print("Time take by find_youtube_url:", end - start)
 
     return first_vid
 
 
 def download_yt(yt_link, track_info):
     """download the video in mp3 format from youtube"""
-    start = timer()
     # TODO: show progres
     # TODO: return True if the video is downloaded, False otherwise
-    print(
-        f"'{track_info['track_title']}' by '{track_info['artist_name']}'..."
-    )
+
     yt = YouTube(yt_link)
     video = yt.streams.filter(only_audio=True).first()
     vid_file = video.download(output_path="../music")
-    end = timer()
 
     # convert the downloaded video to mp3
     base = os.path.splitext(vid_file)[0]
@@ -108,12 +112,11 @@ def download_yt(yt_link, track_info):
     mp4_no_frame.close()
     os.remove(vid_file)
 
-    fsize = round(os.path.getsize(audio_file) / 1024**2, 2)
+    # fsize = round(os.path.getsize(audio_file) / 1024**2, 2)
     set_metadata(track_info, audio_file)
+    console.print("[blue]______________________________________________________________________")
+    # print(f"Download size: {fsize} MB")
 
-    print("=====================================================")
-    print("Time take by download_yt:", end - start)
-    print(f"Download size: {fsize} MB")
 
 
 def set_metadata(metadata, file_path):
@@ -141,4 +144,5 @@ def set_metadata(metadata, file_path):
 
 
 if __name__ == "__main__":
+    console = Console()
     main()
